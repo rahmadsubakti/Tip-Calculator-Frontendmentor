c5af92ceac2817817a407ce3f6254f0a80326685
rahmadsubakti <38337821+rahmadsubakti@users.noreply.github.com>
2022-10-21 09:28:11 +0700

    Fixing the logo

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import MainApp from 'components/MainApp';
 import logo from 'assets/images/logo.svg';
 
 function App() {
-  let bill = 142.55;
-  let tip = 15;
-  let numPeople = 5;
   return (
     <main>
-      <img src={logo} alt="" className="logo"/>
+      <div className="logo">
+        <img src={logo} alt="" className="logo"/>
+      </div>
       <MainApp />
     </main>
   );
