f3eb09e5bf1bd1aa1995ffcf8b216f30c7bc1dfb
rahmadsubakti <38337821+rahmadsubakti@users.noreply.github.com>
2022-10-21 09:32:42 +0700

    Fixing infinity total amout

--- src/components/Amount.js
+++ src/components/Amount.js
@@ -3,10 +3,13 @@ import { InfoMoney } from "./unit";
 
 import { stateContext } from "./AppContext";
 
+// someone please suggest a good name
+const floatValue = value => (isNaN(value) || !isFinite(value)) ? 0 : Math.floor(value * 100) / 100;
+
 function TipAmount() {
   let {bill, tip, numPeople} = useContext(stateContext);
   let value = bill * (tip / 100) / numPeople
-  value = (isNaN(value) || !isFinite(value)) ? 0 : Math.floor(value * 100) / 100;
+  value = floatValue(value);
   return (
     <InfoMoney value={value}>
       <p>Tip Amount<br/><span>/ person</span></p>
@@ -18,7 +21,7 @@ function TotalAmount() {
   let {bill, tip, numPeople} = useContext(stateContext);
   let total = bill + (bill * tip/100);
   let value = total / numPeople;
-  value = isNaN(value) ? 0 : Math.floor(value * 100) / 100;
+  value = floatValue(value);
   return (
     <InfoMoney value={value}>
       <p>Total <br/><span>/ person</span></p>
